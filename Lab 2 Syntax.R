a14e8d76dc4f8e3bade9b4dd03c725f932533902
Franchelle Young <franchelley@gmail.com>
2019-09-11 03:25:51 -0500

    load("C:/Users/Franchelle/Downloads/fish_data.Rdata")

---
+++ Lab 2 Syntax.R
@@ -0,0 +1,70 @@
+x = 7
+y <- 7
+c(1,2,3,4,5,6,7)
+x = as.integer(7)
+x
+class(x)
+is.integer(x)
+"This is an object"
+data <- c("American", "Cheddar", "Swiss","Mozzarella")
+print(data)
+print(is.factor(data))
+factor_data <- factor(data)
+print(factor_data)
+print(is.factor(factor_data))
+%%c(1,2,3,4,5,6,7)
+c(1,2,3,4,5,6,7,8,9,10)
+x = 7
+y = 7
+z = 10
+x <- 7; y <- 7; z <- 10
+print(x,y,z)
+print(x)
+print(y)
+print(z)
+x* y + z
+x* (y + z)
+# the outputs for the two are different because x* y +z is 7x7 plus 10, which equals 59
+# the output for x*(y + z) is 10 + 7 = 17 times 7 = 119
+f <- c(1,2,3,4,5,6,7,8,9,10)
+f
+length(f) <- 11
+f
+3^ 2007
+-3^ 2007
+inf-inf
+0/0
+v
+[v]
+v <- 
+v <- c(1, NA, NULL)
+v
+v[3]
+v[2]
+[[3]]
+[3]
+3
+v[[1]]
+NULL
+NULL
+7 + 18
+41 %% 2
+21 %% 2
+# integer division
+41 %% 2
+# integer division
+21 %% 2
+21 %/% 2
+41 %/% 2
+# exponents
+18 ^3
+# modulus
+71 %% 51
+b
+b[4]
+b[2]
+b[1:3]
+[b,2]
+b,2
+b
+[1,3]
