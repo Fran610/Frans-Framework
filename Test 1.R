2459ad75a870828585abb6b0d99239dcec8af967
Fran610 <54907913+Fran610@users.noreply.github.com>
2019-09-18 15:44:20 -0500

    Add files via upload

---
+++ Test 1.R
@@ -0,0 +1,61 @@
+c <- c(5,6,7,8,9,10,11,12,13,14,15)
+c <- c(6,7,8,9,10,12,13,14,15)
+c
+str(c)
+f <- 5
+r <- 11
+f < r
+f >r
+f = r
+c <- c(7,8,9,12,14,15)
+str(c)
+x <- 6
+y <- 10
+z <- 13
+((z + x) * (z + y))/2
+10* (x-y)
+# The R operator for not in Nan
+11%%5
+#This operator is giving the modulus, which the remainder after division
+11^5
+#This operator is giving the exponential value of this equation
+11%/%5
+#This operator is finding the integer
+%myop% <- function(r,f) {r=11, f=5}
+"%myop%" <- function(r,f) {r =11, f=5}
+myop <- function(r,f) {r = 11,f= 5}
+ d <- "dog"
+p -> "pig" 
+g <- 7 , q <- 9 , w <- 16
+print(g)
+print(q)
+print(w)
+2^2024
+2^-2024
+-2^2024
+0/0
+ v <- c(1,2,3,4,5)
+ v <- c(1,2, NA, 4, 5, NA)
+ switcheroo.if.then <- function(x) {if (x == "a") "camel"
+   if (x == "b") "bear"
+   if (x == "c") "cat"
+   else "dog" }
+ x == a
+x = a 
+switcheroo.if.then(c)
+siwtcheroo.if.then(d)
+switcheroo.if.then(s)
+a <- "ant"
+b <- 4
+r <- 8.8
+b <- 435
+a.ray = array(data = 1:4, dim = c(4,3,2))
+matrix(data= 1:5, nrow = 5, ncol=2)
+matrix$[4,1]
+
+load("test1_data.Rdata")
+nrow("test1_data.Rdata"")
+#There are 503441 rows and 18 columns
+
+head(test)
+
